fb663a79d8733f245b5082b817304fcbed339196
kharenzze <str237@gmail.com>
2017-12-18 23:50:21 +0100

    Added several scripts

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour {
+
+    public Transform palm = null;
+    public Transform thumb = null;
+    public Transform index = null;
+    public Transform middle = null;
+    public Transform pinky = null;
+    public Transform ring = null;
+
+    private void setNodeRotation(Transform node, float rotation) {
+        Vector3 euler = node.rotation.eulerAngles;
+        node.rotation = new Vector3(euler.x, 90 * rotation, euler.z);
+    }
+}
