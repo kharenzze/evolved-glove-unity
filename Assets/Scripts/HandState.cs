79dd42fdca62f1729c92d54f5da54462d8d221bf
kharenzze <str237@gmail.com>
2017-12-19 00:37:45 +0100

    Init class handstate

--- Assets/Scripts/HandState.cs
+++ Assets/Scripts/HandState.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandState : MonoBehaviour {
+public class HandState {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float[] thumb;
+    public float[] index;
+    public float[] middle;
+    public float[] pinky;
+    public float[] ring;
+    public Vector3 palm; 
+
+    public HandState() {
+        thumb = new float[3];
+        index = new float[3];
+        middle = new float[3];
+        pinky = new float[3];
+        ring = new float[3];
+    }
 }
