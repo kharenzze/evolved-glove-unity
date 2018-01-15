2b7c9338e6d736fbd5d617a7705b623584568cd9
kharenzze <str237@gmail.com>
2018-01-16 00:59:11 +0100

    Refactor, to use different ranges for angles

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -17,16 +17,22 @@ public class HandController : MonoBehaviour {
 
     private const float _rate = .5f;
 
-    private void setNodeRotation(Transform node, float rotationRate) {
-        float angle = (-100 * rotationRate) + 10;
+    private void setNodeRotation(Transform node, float angle) {
         var rotation = Quaternion.Euler(0, angle, 0); // +90 degrees Y rotation
         node.localRotation = Quaternion.Slerp(node.localRotation, rotation,  _rate);
     }
 
-    private void setThumbRotation(Transform thumb, float rotationRate) {
-        var rotation = Quaternion.Euler(0, 0, 90f * rotationRate); 
+    private float baseNodeAngle(float rotationRate) {
+        return (-110 * rotationRate) + 20;
+    }
+
+    private float middleNodeAngle(float rotationRate) {
+        return -90 * rotationRate;
+    }
+
+    private void setThumbRotation(Transform thumb, float angle) {
+        var rotation = Quaternion.Euler(0, 0, -angle); 
         thumb.localRotation = Quaternion.Slerp(thumb.localRotation, rotation,  _rate);
-        print(thumb.localRotation.eulerAngles);
     }
 
     private void Update() {
@@ -42,24 +48,24 @@ public class HandController : MonoBehaviour {
     }
 
     private void loadHandState(HandState hs) {
-        setNodeRotation(pinky, hs.pinky[0]);
-        setNodeRotation(pinky.GetChild(0), hs.pinky[1]);
-        setNodeRotation(pinky.GetChild(0).GetChild(0), hs.pinky[1]);
+        setNodeRotation(pinky, baseNodeAngle(hs.pinky[0]));
+        setNodeRotation(pinky.GetChild(0), middleNodeAngle(hs.pinky[1]));
+        setNodeRotation(pinky.GetChild(0).GetChild(0), middleNodeAngle(hs.pinky[1]));
         
-        setNodeRotation(ring, hs.ring[0]);
-        setNodeRotation(ring.GetChild(0), hs.ring[1]);
-        setNodeRotation(ring.GetChild(0).GetChild(0), hs.ring[1]);
+        setNodeRotation(ring, baseNodeAngle(hs.ring[0]));
+        setNodeRotation(ring.GetChild(0), middleNodeAngle(hs.ring[1]));
+        setNodeRotation(ring.GetChild(0).GetChild(0), middleNodeAngle(hs.ring[1]));
         
-        setNodeRotation(middle, hs.middle[0]);
-        setNodeRotation(middle.GetChild(0), hs.middle[1]);
-        setNodeRotation(middle.GetChild(0).GetChild(0), hs.middle[1]);  
+        setNodeRotation(middle, baseNodeAngle(hs.middle[0]));
+        setNodeRotation(middle.GetChild(0), middleNodeAngle(hs.middle[1]));
+        setNodeRotation(middle.GetChild(0).GetChild(0), middleNodeAngle(hs.middle[1]));  
         
-        setNodeRotation(index, hs.index[0]);
-        setNodeRotation(index.GetChild(0), hs.index[1]);
-        setNodeRotation(index.GetChild(0).GetChild(0), hs.index[1]);
+        setNodeRotation(index, baseNodeAngle(hs.index[0]));
+        setNodeRotation(index.GetChild(0), middleNodeAngle(hs.index[1]));
+        setNodeRotation(index.GetChild(0).GetChild(0), middleNodeAngle(hs.index[1]));
 
-        setThumbRotation(thumb.GetChild(0), hs.thumb[0]);
-        setThumbRotation(thumb.GetChild(0).GetChild(0), hs.thumb[1]);
+        setThumbRotation(thumb.GetChild(0), baseNodeAngle(hs.thumb[0]));
+        setThumbRotation(thumb.GetChild(0).GetChild(0), baseNodeAngle(hs.thumb[1]));
 
         setPalmReference(hs.palm);
     }
